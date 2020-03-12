99c1004d193747a6b1309e664a073036afe84609
HP <martinsonyedikachi@gmail.com>
2020-03-12 07:41:37 +0100

    Added my algorithm for mining

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -1,2 +1,107 @@
 # Paste your version of blockchain.py from the basic_block_gp
 # folder here
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+
+from flask import Flask, jsonify, request
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+
+        block = {
+
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        current_transactions = []
+        self.chain.append(block)
+        return block
+
+
+    def hash(self, block):
+        string_block = json.dumps(block, sort_keys=True)
+        raw_hash = hashlib.sha256(string_block.encode())
+        hex_hash = raw_hash.hexdigest()
+        return hex_hash
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+
+    @staticmethod
+    def valid_proof(block_string, proof):
+        guess = f"{block_string}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:6] == "000000"
+
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    data = request.get_json()
+
+    if "proof" not in data or "id" not in data:
+        response = {
+            "Error": "Does not contain both a proof and an id"
+        }
+        return jsonify(response), 200
+
+    proof = data["proof"]
+
+    block_string = json.dumps(blockchain.last_block, sort_keys=True)
+
+    if blockchain.valid_proof(block_string, proof):
+        previous_hash = blockchain.hash(blockchain.last_block)
+        block = blockchain.new_block
+
+        response = {
+           'message': 'We got it mined!!'
+        }
+
+        return jsonify(response), 200
+    else:
+        response = {
+            'message': 'Not good enough'
+        }
+        return jsonify(response), 400
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+@app.route('/last_block', methods=['GET'])
+def last_block():
+    response = {
+        "last_block": blockchain.chain[-1]
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
